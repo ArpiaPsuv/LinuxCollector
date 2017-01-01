a510ff0b8bf50571059d1aa1f0041cc32bb2c254
leetspeaker <cdalvarezf@gmail.com>
2017-01-01 00:05:05 -0400

    Update

--- ServerDaemon/main.cpp
+++ ServerDaemon/main.cpp
@@ -1,9 +1,67 @@
-#include <iostream>
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
 
-using namespace std;
-
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    const char * mensajerespuesta = "<html><head><title>TITULO!</title></head><body><h1>TEXTO DE PRUEBA</h1></body></html>";
+    int socket_desc, client_sock, c, read_size;
+    struct sockaddr_in server, client;
+    char client_message[2000];
+
+    //Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 1024 );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+
+    //Listen
+    listen(socket_desc, 3);
+    c = sizeof(struct sockaddr_in);
+
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+
+    //Receive a message from client
+    while( (read_size = recv(client_sock, client_message, 2000, 0)) > 0 )
+    {
+        //Send the message back to client
+        puts(client_message);
+        write(client_sock, mensajerespuesta, strlen(client_message));
+    }
+
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+
     return 0;
 }
